18cf35f04bccc5bcd8aaa518b7d8b07f7d6ebd56
IllusionzDev <68855711+IllusionzDev@users.noreply.github.com>
2020-07-30 17:17:36 +0100

    Update randomfeed.java

--- randomfeed.java
+++ randomfeed.java
@@ -10,7 +10,7 @@ import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.plugin.java.JavaPlugin;
 
-import java.util.HashMap;
+import java.util.Map;
 import java.util.Random;
 import java.util.UUID;
 
